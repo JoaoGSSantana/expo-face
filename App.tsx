36506a4bf8778244a58bf8f4523a9198c259a55e
João Gabriel Santana <joao.santana09@outlook.com>
2023-12-03 12:12:11 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,69 @@
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Camera } from 'expo-camera';
+
+import {CameraType, FaceDetectionResult} from 'expo-camera/build/Camera.types'
+
+import * as FaceDetector from 'expo-face-detector';
 
 export default function App() {
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  console.log(permission)
+
+function handleFacesDetected({faces}: FaceDetectionResult) {
+  if(faces.length === 0) {
+    console.log('No faces detected');
+  }else {
+    faces.map((value, index) => {
+      const face = value as FaceDetector.FaceFeature;
+
+      console.log(face)
+    })
+  }
+}
+
+if (!permission) {
+  // Camera permissions are still loading
+  return <View />;
+}
+
+if (!permission.granted) {
+  // Camera permissions are not granted yet
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+      <Button onPress={requestPermission} title="grant permission" />
+    </View>
+  );
+}
+
+  return (
+    <View style={styles.container}>
+    <Camera
+    style={styles.camera}
+    ratio="16:9" 
+    type={CameraType.front}
+    onFacesDetected={({faces}) => {
+      if(faces.length === 0) {
+        console.log('No faces detected');
+      }else {
+        faces.map((value, index) => {
+          const face = value as FaceDetector.FaceFeature;
+    
+          console.log(face.smilingProbability)
+        })
+      }
+    }}
+    faceDetectorSettings={{
+      mode: FaceDetector.FaceDetectorMode.fast,
+      detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
+      runClassifications: FaceDetector.FaceDetectorClassifications.all,
+      minDetectionInterval: 100,
+      tracking: true,
+    }}
+  />
     </View>
   );
 }
@@ -13,8 +71,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  camera: {
+    flex: 1
+  }
 });
